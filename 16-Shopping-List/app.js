39627d2b9dd7f66a1ee95e33c3d4adbee13f8b55
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2023-12-22 15:52:34 +0100

    Den nästan färdiga lösningen för Shopping-List

--- 16-Shopping-List/app.js
+++ 16-Shopping-List/app.js
@@ -10,7 +10,13 @@ const input = document.querySelector('#item-input');
 const list = document.querySelector('#item-list');
 // Få en referens til "Töm Allt" knappen...
 const clearButton = document.querySelector('#clear');
+// Få en referens till filter inmatningen...
+const filterInput = document.querySelector('#filter');
 
+// Starta applikationen med rätt inställningar...
+const initApp = () => {
+  updateUI();
+};
 // 2. Skapa en händelsehanteringsfunktion...
 const handleAddGrocery = (e) => {
   e.preventDefault();
@@ -21,26 +27,46 @@ const handleAddGrocery = (e) => {
   if (grocery.trim().length === 0) {
     const errorMsg = createErrorMessage('Du måste ange en vara!');
     document.querySelector('.message-container').appendChild(errorMsg);
-    input.value = '';
+
+    // Automatiskt ta bort felmeddelandet efter 3sek(3000)...
+    setTimeout(() => {
+      document.querySelector('#error-message').remove();
+    }, 3000);
+
+    updateUI();
     return;
   }
 
-  if (document.querySelector('#error-message')) {
-    document.querySelector('#error-message').remove();
-  }
-
-  addGroceryToDOM(grocery);
-  input.value = '';
+  // Spara ner till localstorage...
+  addToStorage(grocery);
+  updateUI();
 };
 
 const handleClearList = () => {
-  while (list.firstChild) list.removeChild(list.firstChild);
+  clearStorage();
+  updateUI();
 };
 
 const handleClickGrocery = (e) => {
   if (e.target.parentElement.classList.contains('btn-link')) {
-    e.target.parentElement.parentElement.remove();
+    removeFromStorage(e.target.parentElement.parentElement.textContent);
   }
+
+  updateUI();
+};
+
+const handleFilterGroceries = (e) => {
+  const groceries = document.querySelectorAll('#item-list li');
+  const value = e.target.value;
+
+  groceries.forEach((item) => {
+    const itemValue = item.firstChild.textContent.toLowerCase();
+    if (itemValue.indexOf(value) !== -1) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 };
 
 // Refactoring av dom manipulering till separat funktioner...
@@ -49,8 +75,6 @@ const addGroceryToDOM = (grocery) => {
   item.appendChild(document.createTextNode(grocery));
   item.appendChild(createIconButton('btn-link text-red'));
   list.appendChild(item);
-  // const button = createIconButton('btn-link text-red');
-  // item.appendChild(button);
 };
 
 const createIconButton = (classes) => {
@@ -77,8 +101,62 @@ const createErrorMessage = (text) => {
   return div;
 };
 
+const updateUI = () => {
+  input.value = '';
+  // Radera det som finns i DOM...
+  // Alltså listan med varor...
+  while (list.firstChild) list.removeChild(list.firstChild);
+
+  const groceryList = getFromStorage();
+  groceryList.forEach((grocery) => addGroceryToDOM(grocery));
+
+  if (groceryList.length === 0) {
+    clearButton.style.display = 'none';
+    filterInput.style.display = 'none';
+  } else {
+    clearButton.style.display = 'block';
+    filterInput.style.display = 'block';
+  }
+};
+
+// Arbeta med LocalStorage...
+// Lägga till...
+const addToStorage = (grocery) => {
+  const groceries = getFromStorage();
+  groceries.push(grocery);
+  // setItem som tar två argument 1. namnet på nyckeln(key) 2. värdet som sträng...
+  localStorage.setItem('groceries', JSON.stringify(groceries));
+};
+
+// Hämta ut...
+const getFromStorage = () => {
+  let groceries;
+
+  if (localStorage.getItem('groceries') === null) {
+    groceries = [];
+  } else {
+    groceries = JSON.parse(localStorage.getItem('groceries'));
+  }
+
+  return groceries;
+};
+
+// Tar bort en vara...
+const removeFromStorage = (grocery) => {
+  let groceries = getFromStorage();
+  groceries = groceries.filter((item) => item !== grocery);
+  localStorage.setItem('groceries', JSON.stringify(groceries));
+};
+
+// Tömmer hela LocalStorage...
+const clearStorage = () => {
+  localStorage.removeItem('groceries');
+};
+
 // 3. Kopplar händelse till html elementen...
 // Händelsehantering...
+document.addEventListener('DOMContentLoaded', initApp);
 form.addEventListener('submit', handleAddGrocery);
 clearButton.addEventListener('click', handleClearList);
 list.addEventListener('click', handleClickGrocery);
+filterInput.addEventListener('input', handleFilterGroceries);
